a826972713f9857b4d1642090ae4fc600116769d
Sergey Melnik <sergey.melnik@commercetools.de>
2017-09-21 15:07:55 +0200

    Update tests, add Auth testing

--- monit_exporter_test.go
+++ monit_exporter_test.go
@@ -12,6 +12,9 @@ import (
 
 const (
 	monitStatus = `<?xml version="1.0" encoding="ISO-8859-1"?><monit><server><id>acfbb9e9118e68d3754761a79d3aae16</id><incarnation>1504605214</incarnation><version>5.23.0</version><uptime>136736</uptime><poll>60</poll><startdelay>0</startdelay><localhostname>fc566edc8b68</localhostname><controlfile>/opt/monit/etc/monitrc</controlfile><httpd><address>172.17.0.2</address><port>2812</port><ssl>0</ssl></httpd></server><platform><name>Linux</name><release>4.9.27-moby</release><version>#1 SMP Thu May 11 04:01:18 UTC 2017</version><machine>x86_64</machine><cpu>4</cpu><memory>2046768</memory><swap>1048572</swap></platform><service type="5"><name>fc566edc8b68</name><collected_sec>1505209672</collected_sec><collected_usec>23215</collected_usec><status>0</status><status_hint>0</status_hint><monitor>1</monitor><monitormode>0</monitormode><onreboot>0</onreboot><pendingaction>0</pendingaction><system><load><avg01>0.00</avg01><avg05>0.00</avg05><avg15>0.00</avg15></load><cpu><user>0.1</user><system>0.1</system><wait>0.1</wait></cpu><memory><percent>6.5</percent><kilobyte>133628</kilobyte></memory><swap><percent>0.0</percent><kilobyte>0</kilobyte></swap></system></service></monit>`
+	monitServiceName = `fc566edc8b68`
+	monitUser = `user`
+	monitPassword = `password`
 )
 
 func TestMonitStatus(t *testing.T) {
@@ -21,7 +24,7 @@ func TestMonitStatus(t *testing.T) {
 	})
 	server := httptest.NewServer(handler)
 	config := ParseConfig()
-	config.listen_address = server.URL
+	config.monit_scrape_uri = server.URL
 	e, err := NewExporter(config)
 	if err != nil {
 		t.Error("Unexpected error during exporter creation")
@@ -37,17 +40,14 @@ func TestFieldsParsing(t *testing.T) {
 	if err != nil {
 		t.Error("Unable to parse XML:", err)
 	}
-	t.Log(parsedData)
+	if parsedData.MonitServices[0].Name != monitServiceName {
+		t.Errorf("want Name  %d, have %d.", monitServiceName, parsedData.MonitServices[0].Name)
+	}
 }
 
 func TestMonitUnavailable(t *testing.T) {
 	mConfig := &Config{
-		listen_address: "http://localhost:93",
-		metrics_path: "/metrics",
-		ignore_ssl: true,
-		monit_scrape_uri: "http://localhost:2812/status",
-		monit_user: "user",
-		monit_password: "password",
+		monit_scrape_uri: "http://localhost:1/status",
 	}
 	e, err := NewExporter(mConfig)
 	if err != nil {
@@ -77,9 +77,48 @@ func TestHttpQueryExporter(t *testing.T) {
 	if want, have := http.StatusOK, resp.StatusCode; want != have {
 		t.Errorf("want /metrics status code %d, have %d. Body:\n%s", want, have, b)
 	}
+}
 
+func AuthHandler(w http.ResponseWriter, r *http.Request)  {
+	user, pass, _ := r.BasicAuth()
+	if user == monitUser && pass == monitPassword {
+		w.Write([]byte(monitStatus))
+
+	} else {
+		http.Error(w, "Unauthorized.", 401)
+	}
 }
 
 func TestBasicAuth(t *testing.T) {
+	handler := http.HandlerFunc(AuthHandler)
+	server := httptest.NewServer(handler)
+	config := ParseConfig()
+	config.monit_scrape_uri = server.URL
+	config.monit_user = monitUser
+	config.monit_password = monitPassword
+	e, err := NewExporter(config)
+	if err != nil {
+		t.Error("Unexpected error during exporter creation")
+	}
+	err = e.scrape()
+	if err != nil {
+		t.Error("Unexpected execution error:", err)
+	}
+}
 
+func TestBasicAuthFail(t *testing.T) {
+	handler := http.HandlerFunc(AuthHandler)
+	server := httptest.NewServer(handler)
+	config := ParseConfig()
+	config.monit_scrape_uri = server.URL
+	config.monit_user = monitUser
+	config.monit_password = monitPassword + "qwe"
+	e, err := NewExporter(config)
+	if err != nil {
+		t.Error("Unexpected error during exporter creation")
+	}
+	err = e.scrape()
+	if err == nil {
+		t.Error("Unexpected execution success:")
+	}
 }
